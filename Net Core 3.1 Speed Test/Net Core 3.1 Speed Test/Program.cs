8d09ef78c14e5db87660ec8e6e198d43e1848a47
kelojok <55785050+kelojok@users.noreply.github.com>
2021-01-27 10:44:22 +0100

    Added StringStartsWith method
    This method checks if the "string starts with" plenty of times

--- Net Core 3.1 Speed Test/Net Core 3.1 Speed Test/Program.cs
+++ Net Core 3.1 Speed Test/Net Core 3.1 Speed Test/Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
 
 namespace Net_Core_3._1_Speed_Test
 {
-    class Program
+    
+    
+    public class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
-            Console.WriteLine("Hello World!");
+            var summary = BenchmarkRunner.Run<StringStartsWith>();
         }
+
+
     }
+
+    [RPlotExporter, RankColumn]
+    public class StringStartsWith
+    {
+        public string alphabetWithNumbers = "abcdefghijklmnopqrstuvwxyzåäö1234567890";
+        public bool startsWith;
+
+        [Benchmark]
+        public void StartsWithExecution() 
+        {
+            for (int i = 0; i < 1000000000; i++)
+            {
+                alphabetWithNumbers.StartsWith("abcdefghijklmnopqrstuvwxyzåäö1234567890");
+            }
+        }  
+    }
+
+
 }
